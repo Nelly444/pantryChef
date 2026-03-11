1df799bcb40e247d845eaecec47a7eaa24872244
Nelly444 <nelsonsupriyasilp111@gmail.com>
2026-03-11 16:42:07 -0700

    created calculate_nutrition method

--- recipe.py
+++ recipe.py
@@ -1,5 +1,6 @@
 from spoonacular import find_ingredients, get_recipe_info
 
+# Calculate match percentage for pantry items
 def calculate_match(pantry_items):
     ingredients = list(pantry_items)
 
@@ -14,3 +15,17 @@ def calculate_match(pantry_items):
             best_pct = match_percentage
             best = recipe
     return best
+
+# Calculate nutrition information for a recipe
+def calculate_nutrition(recipe, servings):
+    nutrients = recipe['nutrition']['nutrients']
+    nutrient_map = {n["name"].lower(): n["amount"] for n in nutrients}
+    result = {
+        "calories": nutrient_map.get("calories", 0) * servings,
+        "protein": nutrient_map.get("protein", 0) * servings,
+        "fat": nutrient_map.get("fat", 0) * servings,
+        "carbs": nutrient_map.get("carbohydrates", 0) * servings
+    }
+    return result
+
+
