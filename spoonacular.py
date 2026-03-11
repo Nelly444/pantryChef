75ff189b50f280b5f4a7948d6719d035c1abc29a
Nelly444 <nelsonsupriyasilp111@gmail.com>
2026-03-11 16:17:27 -0700

    Created get_recipe_info method

--- spoonacular.py
+++ spoonacular.py
@@ -6,7 +6,8 @@ load_dotenv() # this reads the .env file
 
 API_KEY = os.getenv("SPOONACULAR_API_KEY")
 
-def findIngredients(ingredients):
+# Find recipes by ingredients
+def find_ingredients(ingredients):
     url = "https://api.spoonacular.com/recipes/findByIngredients"
     
     params = {
@@ -20,3 +21,16 @@ def findIngredients(ingredients):
     response = requests.get(url, params=params)
     response.raise_for_status() #raises an error if request fails
     return response.json()
+
+# Get recipe information by ID
+def get_recipe_info(id):
+    url = f"https://api.spoonacular.com/recipes/{id}/information"
+    params = {
+        "apiKey": API_KEY,
+        "includeNutrition": True
+    }
+
+    response = requests.get(url, params=params)
+    response.raise_for_status() #raises an error if request fails
+    return response.json()
+
