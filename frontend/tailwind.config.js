2ac5c5cd0438d6609bcb9108be6c581e9145a561
Nelly444 <nelsonsupriyasilp111@gmail.com>
2026-05-14 15:12:14 -0700

    Background changes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        cream:   { DEFAULT: '#f4f0e6', dark: '#e2ead4' },
+        cream:   { DEFAULT: '#ede6d0', dark: '#e2ead4' },
         bark:    { DEFAULT: '#2c1f0e', light: '#5c3d1e' },
         sage:    { DEFAULT: '#7a9e6e', light: '#a8c49a', dark: '#4e7a42' },
         forest:  { DEFAULT: '#3d5c2e', light: '#5c7a42', dark: '#2a4020' },
